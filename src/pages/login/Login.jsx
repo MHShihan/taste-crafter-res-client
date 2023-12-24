84eecdae2e0c4f5cae58e0f3bb826af8a1c23943
MHShihan <mahmodul.no.1@gmail.com>
2023-12-24 23:44:55 +0600

    Capthca has added to the login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,39 @@
+import { useEffect, useRef, useState } from "react";
 import loginBg from "../../assets/others/authentication.png";
 import loginImg from "../../assets/others/authentication2.png";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 
 const Login = () => {
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+
   const handleLogin = (e) => {
     e.preventDefault();
     const form = e.target;
     const email = form.email.value;
     const password = form.password.value;
     console.log({ email, password });
+    // form.reset();
+    // setDisabled(true);
   };
 
+  const handleValidateCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
   return (
     <div
       className="max-w-7xl mx-auto p-1 "
@@ -48,6 +72,25 @@ const Login = () => {
                   className="input input-bordered"
                   required
                 />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  type="text"
+                  ref={captchaRef}
+                  name="captcha"
+                  placeholder="type the captcha above"
+                  className="input input-bordered"
+                  required
+                />
+                <button
+                  onClick={handleValidateCaptcha}
+                  className="btn btn-outline btn-xs my-2"
+                >
+                  Validate
+                </button>
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">
                     Forgot password?
@@ -55,9 +98,12 @@ const Login = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn bg-[#D1A054B3] text-white font-medium font-inter text-xl hover:bg-[#d19f54cc] hover:scale-105">
-                  Login
-                </button>
+                <input
+                  disabled={disabled}
+                  type="submit"
+                  className="btn bg-[#D1A054B3] text-white font-medium font-inter text-xl hover:bg-[#d19f54cc] hover:scale-105"
+                  value="Login"
+                />
               </div>
             </form>
           </div>
